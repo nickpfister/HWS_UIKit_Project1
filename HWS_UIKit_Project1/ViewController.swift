e567442c1bdf9be63f830662dc9941c062ece833
Nick Pfister <7969388+nickpfister@users.noreply.github.com>
2021-11-20 13:27:36 -0800

    Challenges

--- HWS_UIKit_Project1/ViewController.swift
+++ HWS_UIKit_Project1/ViewController.swift
@@ -27,6 +27,10 @@ class ViewController: UITableViewController {
             }
         }
         
+        pictures.sort {
+            $0 < $1
+        }
+        
         print(pictures)
     }
     
@@ -43,6 +47,7 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
             vc.selectedImage = pictures[indexPath.row]
+            vc.title = "Picture \(indexPath.row + 1) of \(pictures.count)"
             navigationController?.pushViewController(vc, animated: true)
         }
     }

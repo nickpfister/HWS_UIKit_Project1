b5b8252df2f32c0eff196a8199b4dc19850ad92f
Nick Pfister <7969388+nickpfister@users.noreply.github.com>
2021-12-03 16:51:28 -0800

    Completed project 2

--- HWS_UIKit_Project1/DetailViewController.swift
+++ HWS_UIKit_Project1/DetailViewController.swift
@@ -15,10 +15,26 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
 
         navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(shareTapped)
+        )
         
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
